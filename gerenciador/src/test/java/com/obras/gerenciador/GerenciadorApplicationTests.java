9b9a886578fafcbbdd40c0431240c0f795e905fe
AdrianoAmaroP <adriano.amaro.pignata@gmail.com>
2024-06-14 16:05:39 -0300

    projeto start

---
+++ gerenciador/src/test/java/com/obras/gerenciador/GerenciadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obras.gerenciador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

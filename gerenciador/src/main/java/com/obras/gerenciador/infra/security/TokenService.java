da6c1a50f6cb3b0d13d3b9e2b93e0ffb026f2675
AdrianoAmaroP <adriano.amaro.pignata@gmail.com>
2024-06-18 15:44:05 -0300

    ajustando TokenService

--- gerenciador/src/main/java/com/obras/gerenciador/infra/security/TokenService.java
+++ gerenciador/src/main/java/com/obras/gerenciador/infra/security/TokenService.java
@@ -1,9 +1,13 @@
 package com.obras.gerenciador.infra.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
 import com.obras.gerenciador.domain.login.entities.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
@@ -15,19 +19,19 @@ public class TokenService {
 
     public String gerarToken(Usuario usuario){
         try {
-            Algorithm algorithm = Algorithm.HMA256(secrete);
+            Algorithm algorithm = Algorithm.HMAC256(secret);
             String token = JWT.create()
                     .withIssuer("API gerenciador.obras")
-                    .withSubkect(usuario.getNome())
+                    .withSubject(usuario.getNome())
                     .withExpiresAt(dataExpira())
                     .sign(algorithm);
-
+            return token;
         } catch (JWTCreationException exception){
             throw new RuntimeException("Erro ao gerar Token", exception);
         }
     }
 
-    private Object dataExpira() {
+    private Instant dataExpira() {
         return LocalDateTime.now().plusHours(6).toInstant(ZoneOffset.of("-03:00"));
     }
 }

373a0f15b2ac1ac1825b4e1921b74e7b2152a528
AdrianoAmaroP <adriano.amaro.pignata@gmail.com>
2024-06-19 17:34:30 -0300

    adicionando validação do token

--- gerenciador/src/main/java/com/obras/gerenciador/infra/security/SecurityConfigurations.java
+++ gerenciador/src/main/java/com/obras/gerenciador/infra/security/SecurityConfigurations.java
@@ -1,7 +1,9 @@
 package com.obras.gerenciador.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,17 +12,24 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
 
+    @Autowired
+    private SecurityFilter myFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http ) throws Exception{
         http
                 .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(aut -> aut
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .anyRequest().authenticated()
+                ).addFilterBefore(myFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
--- gerenciador/src/main/java/com/obras/gerenciador/infra/security/TokenService.java
+++ gerenciador/src/main/java/com/obras/gerenciador/infra/security/TokenService.java
@@ -1,8 +1,10 @@
 package com.obras.gerenciador.infra.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.obras.gerenciador.domain.login.entities.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -28,6 +30,21 @@ public class TokenService {
             return token;
         } catch (JWTCreationException exception){
             throw new RuntimeException("Erro ao gerar Token", exception);
+        }
+    }
+
+    public String getSubject(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    // specify any specific claim validations
+                    .withIssuer("API gerenciador.obras")
+                    // reusable verifier instance
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT inválido ou expirado!", exception);
         }
     }
 

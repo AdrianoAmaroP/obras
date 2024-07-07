0ddd31aadf7fe7a0c1e6f496d1bb21341ca51ce5
AdrianoAmaroP <adriano.amaro.pignata@gmail.com>
2024-07-07 14:55:33 -0300

    configrando acesso ao cors

---
+++ src/main/java/com/obras/gerenciador/infra/web/CorsConfigurations.java
@@ -0,0 +1,26 @@
+package com.obras.gerenciador.infra.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfigurations implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**")
+                        .allowedOrigins("https://myobras.vercel.app", "http://localhost:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
